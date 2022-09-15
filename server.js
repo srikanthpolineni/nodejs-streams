8a0c1011c592c8a2fe28c703468377a8c798c5dd
Srikanth Polineni <polineni.srikanth@gmail.com>
2022-09-15 16:35:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -50,6 +50,79 @@ app.post('/api2', function (req, res) {
     req.pipe(bb);
 });
 
+app.post('/api3', async function (req, res) {
+    try {
+        const bb = busboy({ headers: req.headers });
+        const awaitPayload = new Promise((resolve, reject) => {
+            bb.on('field', (name, val, info) => {
+                resolve(JSON.parse(val));
+            });
+        });
+
+        bb.on('file', (name, file, info) => {
+            function getPreSignedUrl(payload) {
+                return new Promise(async (resolve, reject) => {
+                    try {
+                        const headers = {
+                            'content-type': CONTENT_TYPE_APPLICATION_JSON
+                        }
+                        const preSignedUrlResponse = await axios.post(`http://localhost:8080/preSignedUploadUrl`, payload, headers);
+                        if (preSignedUrlResponse.data) {
+                            resolve(preSignedUrlResponse.data);
+                            return;
+                        }
+                        reject("Error");
+                    } catch (ex) {
+                        reject(ex);
+                    }
+                });
+            }
+
+            function postDocumentToS3({ preSignedUploadUrl }) {
+                return new Promise(async (resolve, reject) => {
+                    try {
+                         const form = new FormData();
+                         form.append('file', file);
+                        var options = {
+                            headers: {
+                                'Content-Type': ' multipart/form-data',
+                                'Content-Length': 0 //add here
+
+                            },
+                            maxContentLength: Infinity,
+                            maxBodyLength: Infinity,
+                        };
+                        const s3Response = await axios.post(preSignedUploadUrl, form, options)
+                        resolve({ status: "ok" });
+                    } catch (e) {
+                        reject(e);
+                    }
+                });
+            }
+            awaitPayload.then(getPreSignedUrl).then(postDocumentToS3).then(function (data) {
+                res.writeHead(200, { Connection: 'close', 'Content-Type': 'application/json' });
+                res.write(JSON.stringify(data));
+                res.end();
+            }).catch(function (err) {
+                const message = "Error";
+                res.writeHead(500, { Connection: 'close', 'Content-Type': 'application/json' });
+                res.write(JSON.stringify({ error: { message } }));
+                res.end()
+            });
+        });
+        bb.on('close', function () {
+
+        });
+
+        bb.on('finish', function () {
+
+        });
+        req.pipe(bb);
+    } catch (ex) {
+        res.status(500).send(ex);
+    }
+});
+
 app.listen(3000, () => {
     console.log('listening on port 3000');
 });
